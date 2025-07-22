8fa9c4913a24ec2a3dc658995a6794f43f0cc4c2
Roberta Nascimento <60294500+RNask@users.noreply.github.com>
2025-07-22 15:29:01 -0300

    first: início do curso

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Metallic Stallion";
+musica1.artista = "Marina";
+musica1.duracao = 401;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Je ne sais quoi";
+musica2.artista = "Marina";
+musica2.duracao = 333;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
