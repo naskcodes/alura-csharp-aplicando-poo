5eb2392dae1751871485d34d432eb79d00f796ee
Roberta Nascimento <60294500+RNask@users.noreply.github.com>
2025-07-31 15:41:24 -0300

    fix: erros criaram a necessidade de pegar o projeto do curso

--- Musica.cs
+++ Musica.cs
@@ -1,22 +1,7 @@
 class Musica
 {
     public string nome;
-    public string artista;
+    public DateTime dataLancamento;
     public int duracao;
-    private bool disponivel;
-
-    public void ExibirFichaTecnica()
-    {
-        Console.WriteLine($"Nome: {nome}");
-        System.Console.WriteLine($"Artista: {artista}");
-        System.Console.WriteLine($"Duração: {duracao}");
-        if (disponivel)
-        {
-            System.Console.WriteLine("Disponível no plano.");
-        }
-        else
-        {
-            System.Console.WriteLine("Adquira o plano Comumusic+");
-        }
-    }
+    public bool disponivel;
 }
---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,31 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    private bool disponivel;
+
+    public void EscreveDisponivel(bool disponibilidade)
+    {
+        disponivel = disponibilidade;
+    }
+
+    public bool LeituraDisponivel()
+    {
+        return disponivel;
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
